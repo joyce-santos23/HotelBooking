73d0022309acc4f82573450c6e7ca86af89a218c
Joyce Santos <joycectba@hotmail.com>
2024-09-03 19:47:05 -0300

    Adicionar arquivos de projeto.

---
+++ BookingService/Core/Domain/Domain/Booking/Ports/IBookingRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Ports
+{
+    public class IBookingRepository
+    {
+    }
+}
---
+++ BookingService/Core/Domain/Domain/Guest/Ports/IGuestRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Guest.Ports
+{
+    public class IGuestRepository
+    {
+    }
+}
